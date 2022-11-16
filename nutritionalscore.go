d33d102d7a38bab92ef04a72e765acb03bb7eff3
Devmustroc <elmustapha.abourar@outlook.com>
2022-11-16 20:41:49 +0100

    main fucn Get NS with a Nutritional Struct

--- nutritionalscore.go
+++ nutritionalscore.go
@@ -23,3 +23,35 @@ type SodiumMilligram float64
 type FruitsPercent float64
 type FibreGram float64
 type ProteinGram float64
+
+type NutritionalData struct {
+	Energy              EnergyKJ
+	Sugars              SugarGram
+	SaturatedfattyAcids SaturatedFattyAcids
+	Sodium              SodiumMilligram
+	Fruits              FruitsPercent
+	Fibre               FibreGram
+	Protein             ProteinGram
+	IsWater             bool
+}
+
+func GetnutritionalScore(n NutritionalData, st ScoreType) NutritionalScore {
+
+	value := 0
+	positive := 0
+	negative := 0
+
+	if st != Water {
+		fruitPoints := n.Fruits.GetPoint()
+		fibrePoints := n.Fibre.GetPoint()
+
+		negative = n.Energy.GetPoint() + n.Sugars.GetPoint() + n.SaturatedFattyAcids.GetPoint() + n.Sodium.GetPoint()
+		positive = n.FibreGetPoint() + n.Protein.GetPoint() + fruitPoints + fibrePoints
+	}
+	return NutritionalScore{
+		Value:     value,
+		Positive:  positive,
+		Negative:  negative,
+		Scoretype: st,
+	}
+}
